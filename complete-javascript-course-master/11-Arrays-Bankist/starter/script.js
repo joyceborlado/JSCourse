4d5beaf70f7e34bd26305cdfc4cdedf9136c1dbb
joyceborlado <joycebondocoy@gmail.com>
2023-10-16 19:04:00 +0800

    creating and filling arrays

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -556,3 +556,20 @@ console.log(movements);
 
 movements.sort((a, b) => b - a);
 console.log(movements);*/
+
+/////// CREATING AND FILLING ARRAYS
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+
+x.fill(1, 3, 5);
+x.fill(1);
+
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
