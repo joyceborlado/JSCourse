2434646ce22227f5eb6e332e50935ef91b8686ca
joyceborlado <joycebondocoy@gmail.com>
2023-10-18 18:33:33 +0800

    1

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -599,25 +599,12 @@ labelBalance.addEventListener('click', function () {
 });*/
 
 //Array exercise 1
-const bankDepsitSum = accounts
+
+const bankDepositSum = accounts
   .flatMap(acc => acc.movements)
   .filter(mov => mov > 0)
   .reduce((sum, cur) => sum + cur, 0);
 
-console.log(bankDepsitSum);
+console.log(bankDepositSum);
 
-//2
-
-// const numDeposits1000 = accounts
-//   .flatMap(acc => acc.movements)
-//   .filter(mov => mov >= 1000).length;
-
-const numDeposits1000 = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
-
-console.log(numDeposits1000);
-
-let a = 10;
-console.log(++a);
-console.log(a);
+// Array exercise 2
