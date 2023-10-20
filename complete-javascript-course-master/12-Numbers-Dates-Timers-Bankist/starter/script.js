1f82cfa0a6e4246edcece8573684276e7591740b
joyceborlado <joycebondocoy@gmail.com>
2023-10-20 19:09:53 +0800

    Check is value is a number

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -271,3 +271,20 @@ console.log(Number.parseInt('e23', 10)); // NaN
 
 console.log(Number.parseInt('   2.5rem')); // 2
 console.log(Number.parseFloat('   2.5rem  ')); // 2.5
+
+// Check if value is NaN
+console.log(Number.isNaN(20)); //false
+console.log(Number.isNaN('20')); //false
+console.log(Number.isNaN(+'20x')); //true
+console.log(Number.isNaN(23 / 0)); //false
+
+// check if value is number
+
+console.log(Number.isFinite(20)); //true
+console.log(Number.isFinite('20')); //false
+console.log(Number.isFinite(+'20x')); //false
+console.log(Number.isFinite(23 / 0)); //false
+
+console.log(Number.isInteger(23)); //true
+console.log(Number.isInteger(23.0)); //true
+console.log(Number.isInteger(23 / 0)); //false
