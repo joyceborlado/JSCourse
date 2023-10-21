27b82e45d755790e5969322f61a7b878df03f07d
joyceborlado <joycebondocoy@gmail.com>
2023-10-21 21:58:35 +0800

    Rounding integers and decimals

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -303,13 +303,38 @@ console.log(Math.max(5, 8, 23, 11, 2)); //23
 console.log(Math.max(5, 8, '23', 11, 2)); //23 (does type coercion)
 console.log(Math.max(5, 8, '23px', 11, 2)); // NaN (does not do parsing)
 
-console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
 
 // Random Numbers
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592... formula for area of a circle
 
 console.log(Math.trunc(Math.random() * 6) + 1);
 
 const randomInt = (min, max) =>
-  Math.trunc(Math.random() * (max - min) + 1) + min;
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
+
+// Rounding Integers
+
+console.log(Math.round(23.3)); //23
+console.log(Math.round(23.9)); //24
+
+console.log(Math.ceil(23.3)); //24
+console.log(Math.ceil(23.9)); //24
+
+console.log(Math.floor(23.3)); //23
+console.log(Math.floor('23.9')); //23
+
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.trunc(-23.3)); //-23
+console.log(Math.floor(-23.3)); //-24
+
+//Rounding decimals
+
+console.log((2.7).toFixed(0)); // 3 (string)
+console.log((2.7).toFixed(3)); //2.700 (string)
+console.log((2.345).toFixed(2)); //2.35 (string)
+console.log(+(2.345).toFixed(2)); // 2.35 (number)
