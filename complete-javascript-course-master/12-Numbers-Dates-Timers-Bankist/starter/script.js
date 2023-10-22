eb2531c37bb808aa2217eb64e24c74bca8061893
joyceborlado <joycebondocoy@gmail.com>
2023-10-22 21:13:22 +0800

    checking if a number is even

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -352,3 +352,10 @@ console.log(6 / 2);
 
 console.log(7 % 2);
 console.log(7 / 2);
+
+// function
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
