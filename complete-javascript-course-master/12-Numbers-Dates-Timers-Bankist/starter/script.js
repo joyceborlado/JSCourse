df5ed9d793feb397b30369dab46ad0a1650b7c29
joyceborlado <joycebondocoy@gmail.com>
2023-10-23 19:55:05 +0800

    remainder operator in bankist app

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -341,21 +341,34 @@ console.log(+(2.345).toFixed(2)); // 2.35 (number)*/
 
 ////// Remainder Operator //////////////
 
-console.log(5 % 2);
+console.log(5 % 2); // 1
 console.log(5 / 2);
+1; //2.5
 
-console.log(8 % 3);
-console.log(8 / 3);
+console.log(8 % 3); // 2
+console.log(8 / 3); // 2.6666...
 
-console.log(6 % 2);
-console.log(6 / 2);
+console.log(6 % 2); // 0
+console.log(6 / 2); // 3
 
-console.log(7 % 2);
-console.log(7 / 2);
+console.log(7 % 2); // 1
+console.log(7 / 2); // 3.5
 
 // function
 
 const isEven = n => n % 2 === 0;
-console.log(isEven(8));
-console.log(isEven(23));
-console.log(isEven(514));
+console.log(isEven(8)); // true
+console.log(isEven(23)); // false
+console.log(isEven(514)); //true
+
+// sample
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orange';
+
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
