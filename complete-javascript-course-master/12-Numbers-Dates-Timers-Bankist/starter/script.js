bb4f6eedd28abb331447e6488fbb5263e0f12cbd
joyceborlado <joycebondocoy@gmail.com>
2023-10-25 19:27:28 +0800

    calculating number of days

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -493,3 +493,12 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);*/
+
+const future = new Date(2037, 10, 19, 15, 13);
+console.log(+future); // in number format
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
