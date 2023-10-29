940e558182007bb784937f59560d82f5deeb3596
joyceborlado <joycebondocoy@gmail.com>
2023-10-29 21:40:36 +0800

    set Timeout

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -567,49 +567,6 @@ console.log(
   new Intl.NumberFormat(navigator.language).format(num)
 );
 
-const ingredients = ['olives', 'spinach'];
-
-const pizzaTimer = setTimeout(
-  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
-  3000,
-  ...ingredients
-);
-
-
-//// CHAR LANG  /////
-
-console.log('Waiting...');
-
-if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
-
-// Set the date we're counting down to
-var countDownDate = new Date('Jan 5, 2024 15:37:25').getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById('demo').innerHTML =
-    days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById('demo').innerHTML = 'EXPIRED';
-  }
-}, 1000);*/
-//==========================================
 //////////Set timeout /////////////////////////////////
 
 setTimeout(code);
@@ -619,7 +576,7 @@ setTimeout(functionRef);
 setTimeout(functionRef, delay);
 setTimeout(functionRef, delay, param1);
 setTimeout(functionRef, delay, param1, param2);
-setTimeout(functionRef, delay, param1, param2, /* …, */ paramN);
+setTimeout(functionRef, delay, param1, param2, /* …, 
 
 //Obsolete. Never USE!
 setTimeout(() => {
@@ -634,4 +591,15 @@ setTimeout(() => {
 
 setTimeout(() => {
   console.log('Delayed for 1 second.');
-}, 1000);
+}, 1000);*/
+
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}🍕`),
+  3000,
+  ...ingredients
+);
+
+console.log('Waiting.....');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
