d08b19bb1a11d055373d39f9c68671e1d75c7d2c
joyceborlado <joycebondocoy@gmail.com>
2023-10-30 22:20:18 +0800

    set time out bankist cont

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -188,22 +188,30 @@ const updateUI = function (acc) {
 };
 
 const startLogOutTimer = function () {
-  // Set time to 5 minutes
-  let time = 100;
-
-  // Call the timer every second
-  setInterval(function () {
+  const tick = function () {
     const min = String(Math.trunc(time / 60)).padStart(2, 0);
     const sec = String(time % 60).padStart(2, 0);
 
     // In each call, print the remaining time to UI
     labelTimer.textContent = `${min}: ${sec}`;
 
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+
     //Decrease 1s
     time--;
+  };
 
-    // When 0 seconds, stop timer and log out user
-  }, 1000);
+  // Set time to 5 minutes
+  let time = 10;
+
+  // Call the timer every second
+  const timer = setInterval(tick, 1000);
 };
 
 ///////////////////////////////////////
@@ -213,9 +221,9 @@ let currentAccount;
 
 //FAKE ALWAYS LOGGED IN
 
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 // month/day/year
 
