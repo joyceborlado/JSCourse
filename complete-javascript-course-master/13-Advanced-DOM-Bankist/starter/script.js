39fd7a0afacb990b53976c8415e08d6c452c267c
joyceborlado <joycebondocoy@gmail.com>
2023-11-04 20:27:39 +0800

    removeEventListener

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -153,11 +153,15 @@ btnScrollTo.addEventListener('click', function (e) {
 
 const h1 = document.querySelector('h1');
 
-h1.addEventListener('mouseenter', function (e) {
+const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
-});
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 //old way
-h1.onmouseenter = function (e) {
-  alert('onmouseenter: Great! You are reading the heading :D');
-};
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
