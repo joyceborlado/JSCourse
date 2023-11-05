6562ab517978742c04bd8c8b781c841672f0774d
joyceborlado <joycebondocoy@gmail.com>
2023-11-05 09:55:34 +0800

    page navigation

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -63,8 +63,17 @@ btnScrollTo.addEventListener('click', function (e) {
   // Modern Way
   section1.scrollIntoView({ behavior: 'smooth' });
 });
-
+////////////////////////////////////////////
 // Page navigation
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
