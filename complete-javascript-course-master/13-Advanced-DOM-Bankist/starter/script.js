be1cf902d00d6dc0dadf584ceb2b2112e22b650b
joyceborlado <joycebondocoy@gmail.com>
2023-11-09 20:31:24 +0800

    btn next slide

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -232,12 +232,30 @@ imgTargets.forEach(img => imgObserver.observe(img));
 
 // // Slider
 const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
 
 const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.2)';
+slider.style.transform = 'scale(0.4) translateX(-500px)';
 slider.style.overflow = 'visible';
 
 slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+// Next Slide
+btnRight.addEventListener('click', function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+  );
+});
 
 // 0%, 100%, 200%, 300%
 
