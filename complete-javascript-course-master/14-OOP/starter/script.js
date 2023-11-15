db465f399627eb60253eabeaba0f7e3e8625b6a3
joyceborlado <joycebondocoy@gmail.com>
2023-11-15 21:14:51 +0800

    OOP abstraction

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//////////// LECTURE ///////////////
+/* FOUR FUNDAMENTAL OOP PRINCIPLES
+ 1. ABSTRACTION
+
+Phone {
+    charge
+    volume
+
+    homeBtn() {}
+    volumeBtn() {}
+    screen() {}
+}
+
+hiding details that don't matter
+*/
