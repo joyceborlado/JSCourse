f71863bff72c2599928f74721251b97641f77e35
joyceborlado <joycebondocoy@gmail.com>
2023-11-15 21:21:01 +0800

    encapsulation

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -14,4 +14,27 @@ Phone {
 }
 
 hiding details that don't matter
+
+
+2. ENCAPSULATION
+    -keeping properties and methods private inside the class so they are not accessible outside the class
+
+    Hypothetical code:
+
+    User {
+        user
+        private password
+        private email
+
+        login(word) {
+            this.password === word
+        }
+
+        comment(text) {
+            this.checkSPAM(text) {
+                //Verify logic
+            }
+        }
+    }
+ 
 */
