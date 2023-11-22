987570960d58f5e5b4ea0f84a90431df904802f2
joyceborlado <joycebondocoy@gmail.com>
2023-11-22 13:25:51 +0800

    Linking prototypes

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -292,9 +292,23 @@ const Student = function (firstName, birthYear, course) {
   this.course = course;
 };
 
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
 Student.prototype.introduce = function () {
   console.log(`My name is ${this.firstName} and I study ${this.course}`);
 };
 
 const mike = new Student('Mike', 2020, 'Computer Science');
 mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
