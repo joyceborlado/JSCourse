c1c427ba0f971b81b476a9ddcafe28ad8b0efdb4
joyceborlado <joycebondocoy@gmail.com>
2023-11-22 14:09:05 +0800

    Coding challenge #3 solution

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -328,3 +328,44 @@ DATA CAR 1: 'Tesla' going at 120km/h, with a charge of 23%
 
 
 */
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed}km/h with a charge of ${this.charge}%`
+  );
+};
+
+const Tesla = new EV('Tesla', 120, 23);
+Tesla.accelerate();
+Tesla.brake();
+Tesla.accelerate();
+Tesla.chargeBattery(90);
+Tesla.accelerate();
