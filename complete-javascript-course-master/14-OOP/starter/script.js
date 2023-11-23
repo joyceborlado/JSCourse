6ae3531410741a938ffe7402de6b895421d3f5d8
joyceborlado <joycebondocoy@gmail.com>
2023-11-23 20:22:07 +0800

    Inheritance between classes: linking objects

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -452,3 +452,20 @@ const PersonProto = {
 };
 
 const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+
+jay.introduce();
+jay.calcAge();
