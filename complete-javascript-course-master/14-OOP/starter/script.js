f8e9810aa89dd930bcee4cc9d58211e4d32e9605
joyceborlado <joycebondocoy@gmail.com>
2023-11-26 22:41:19 +0800

    Another class example bankist loan feature

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -492,11 +492,25 @@ class Account {
   withdraw(val) {
     this.deposit(-val);
   }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
 acc1.deposit(250);
 acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
 
 console.log(acc1);
+console.log(acc1.pin);
