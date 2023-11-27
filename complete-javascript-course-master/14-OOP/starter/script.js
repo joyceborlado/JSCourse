98c0656e80c945ee2450f4924bf4f04df0ff4d28
joyceborlado <joycebondocoy@gmail.com>
2023-11-27 21:14:07 +0800

    public and private fields

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -473,24 +473,37 @@ jay.calcAge();*/
 ///////////////////////////////////////////////////////////
 // Another class example
 
+// Public fields
+// Private Fields
+// Public Methods
+// Private Methods
+
 class Account {
+  // 1) Public Fields
+  locale = navigator.language;
+
+  // 2) Private Fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    // Protected Property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account , ${owner}`);
   }
 
   // Public Interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
@@ -518,4 +531,7 @@ acc1._approveLoan(1000);
 console.log(acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
+// console.log(acc1.pin);
+
+// console.log(acc1.#movements); //will not be accessed
+// console.log(acc.#pin);
