41776235a4a811657971bea4e6388099ab0a5d25
joyceborlado <joycebondocoy@gmail.com>
2023-12-01 23:15:23 +0800

    using geolocation google map link

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -14,14 +14,13 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // Geolocation
 
 if (navigator.geolocation)
-navigator.geolocation.getCurrentPosition{
-    function(position) {
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
-        console.log(latitude, longitude)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
     }
-    function() {
-        alert('Could not get your position')
-    }
-}
-
+  );
