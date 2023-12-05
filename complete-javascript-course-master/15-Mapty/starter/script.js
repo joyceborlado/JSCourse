012332307a90dbff4af540c6941b08201f920f6d
joyceborlado <joycebondocoy@gmail.com>
2023-12-05 20:23:53 +0800

    change input field

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -46,6 +46,13 @@ if (navigator.geolocation)
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
+  //clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
   // Display marker
 
   console.log(mapEvent);
@@ -64,4 +71,9 @@ form.addEventListener('submit', function (e) {
     )
     .setPopupContent('Workout')
     .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
 });
