965b1b70076811f9124ddbc7afa97d9262064f59
joyceborlado <joycebondocoy@gmail.com>
2023-12-19 19:58:54 +0800

    XMLHTTPRequest

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,11 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest(); // old school way
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.send();
+
+request.addEventListener('load', function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+});
