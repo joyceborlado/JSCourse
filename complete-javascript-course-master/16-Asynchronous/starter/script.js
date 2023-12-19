d215d3076f762c130291b0bfeba7c75b02d0cbdb
joyceborlado <joycebondocoy@gmail.com>
2023-12-19 20:29:55 +0800

    edit const html

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -5,10 +5,27 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 const request = new XMLHttpRequest(); // old school way
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
 request.send();
 
 request.addEventListener('load', function () {
-  const data = JSON.parse(this.responseText);
+  const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+        </div>
+    </article>
+  `;
 });
