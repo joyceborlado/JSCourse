5b92ba180f59061399c1f6130c2529ee9c44cce1
joyceborlado <joycebondocoy@gmail.com>
2023-12-21 20:27:23 +0800

    simplify code

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -96,16 +96,22 @@ getCountryAndNeighbour('philippines');*/
 
 //////////////////////////////////////////
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      renderCountry(data[0]);
-    });
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
 };
 
 getCountryData('portugal');
