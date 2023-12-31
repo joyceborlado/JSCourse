7a94d639951934b767873b9a17c30ad9e92b6b94
joyceborlado <joycebondocoy@gmail.com>
2023-12-31 22:32:04 +0800

    whereamI function

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -62,7 +62,7 @@ const renderError = function (msg) {
 
 getCountryData('portugal');
 getCountryData('usa');
-getCountryData('philippines');*/
+getCountryData('philippines');
 
 /*const getCountryAndNeighbour = function (country) {
   // AJAx call country 1
@@ -97,7 +97,7 @@ getCountryData('philippines');*/
 };
 
 // getCountryAndNeighbour('portugal');
-getCountryAndNeighbour('philippines');*/
+getCountryAndNeighbour('philippines');
 
 //////////////////////////////////////////
 
@@ -150,13 +150,13 @@ btn.addEventListener('click', function () {
   getCountryData('portugal');
 });
 
-getCountryData('Philippines');
+getCountryData('Philippines');*/
 
 /// Coding challenge
 /*
 
 Part I.
-1. Create a function 'whereAmI" which takes as inputs a latituder value (lat) and a longitude value (lng) (these are gps coordinates, examples are below)
+1. Create a function 'whereAmI" which takes as inputs a latitude value (lat) and a longitude value (lng) (these are gps coordinates, examples are below)
 2. Do 'reverse geocoding' of the provided coordinates. reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
 The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating
 3. Once you have the data, take a look at it in the console to see all the attributes that you received about the provided location. Then, using this data, log a message like this to the console: 'You are in Berlin, Germany'
@@ -171,5 +171,6 @@ TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
 TEST COORDINATES 2: 19.037, 72.873
 TEST COORDINATES 2: -33.933, 18.474
 
-
 */
+
+const whereAmI = function (lat, lng) {};
