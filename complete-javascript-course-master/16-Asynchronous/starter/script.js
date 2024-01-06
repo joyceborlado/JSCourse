e3bbfb124fb226bef98fe2d1a16ab374f932b3b5
joyceborlado <joycebondocoy@gmail.com>
2024-01-06 20:57:37 +0800

    create Image

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -362,3 +362,9 @@ const createImage = function (imgPath) {
     });
   });
 };
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    console.log('Image 1 loaded');
+  })
+  .catch(err => console.error(err));
